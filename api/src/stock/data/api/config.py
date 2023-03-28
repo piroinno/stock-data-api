a42290c5d7aba04560b4ce2e4c2d87a6265a870a
piroinno <peter.irojah@piroinno.com>
2023-03-28 17:16:39 +0100

    Using env vars for pydandtic

--- api/src/stock/data/api/config.py
+++ api/src/stock/data/api/config.py
@@ -13,8 +13,7 @@ class Settings(BaseSettings):
         return v
 
     class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+        case_sensitive = True
 
 
 settings = Settings()
